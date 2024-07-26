d8d91aa7327b546dbea853e039ccc640ab578b88
hiroki takada <hirorotaka5@gmail.com>
2024-07-26 10:21:04 +0900

    DBの接続情報の作成

---
+++ app.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/connectdb.js';
+// import userRoutes from './routes/userRoutes.js';
+
+const app = express();
+const port = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// CORS Policy
+app.use(cors());
+
+// JSON
+app.use(express.json());
+
+// Database Connection
+connectDB(DATABASE_URL);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
