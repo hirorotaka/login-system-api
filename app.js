ffe0e6386430673c2ad1c146e40d04aafd2c4cab
hiroki takada <hirorotaka5@gmail.com>
2024-07-26 11:12:12 +0900

    ルーティングの作成

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/connectdb.js';
-// import userRoutes from './routes/userRoutes.js';
-
+import userRoutes from './routes/userRoutes.js';
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -17,6 +17,8 @@ app.use(express.json());
 
 // Database Connection
 connectDB(DATABASE_URL);
+
+app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
